83041024969edc844dc80918b2d62c82d8404fe9
Md Mohib Khan <76740212+MdMohibKhan@users.noreply.github.com>
2021-10-26 17:42:47 +0530

    Add files via upload

---
+++ application/libraries/JwtHandler.php
@@ -0,0 +1,79 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require 'jwtToken/JWT.php';
+require 'jwtToken/ExpiredException.php';
+require 'jwtToken/SignatureInvalidException.php';
+require 'jwtToken/BeforeValidException.php';
+
+use \Firebase\JWT\JWT;
+
+class JwtHandler {
+    protected $jwt_secrect;
+    protected $token;
+    protected $issuedAt;
+    protected $expire;
+    protected $jwt;
+
+    public function __construct()
+    {
+        // set your default time-zone
+        date_default_timezone_set('Asia/Kolkata');
+        $this->issuedAt = time();
+        
+        // Token Validity (3600 second = 1hr)
+        $this->expire = $this->issuedAt + 120;
+        // $this->expire = $this->issuedAt + 1800; // 1/2 hr
+
+
+        // Set your secret or signature
+        $this->jwt_secrect = "this_is_my_secrect";  
+    }
+
+    // ENCODING THE TOKEN
+    public function _jwt_encode_data($iss,$data){
+
+        $this->token = array(
+            //Adding the identifier to the token (who issue the token)
+            "iss" => $iss,
+            "aud" => $iss,
+            // Adding the current timestamp to the token, for identifying that when the token was issued.
+            "iat" => $this->issuedAt,
+            // Token expiration
+            "exp" => $this->expire,
+            // Payload
+            "data"=> $data
+        );
+
+        $this->jwt = JWT::encode($this->token, $this->jwt_secrect);
+        return $this->jwt;
+
+    }
+    
+    //DECODING THE TOKEN
+    public function _jwt_decode_data($jwt_token){
+        try{
+            $decode = JWT::decode($jwt_token, $this->jwt_secrect, array('HS256'));
+            return $decode->data;
+        }
+        catch(\Firebase\JWT\ExpiredException $e){
+            return $e->getMessage();
+        }
+        catch(\Firebase\JWT\SignatureInvalidException $e){
+            return $e->getMessage();
+        }
+        catch(\Firebase\JWT\BeforeValidException $e){
+            return $e->getMessage();
+        }
+        catch(\DomainException $e){
+            return $e->getMessage();
+        }
+        catch(\InvalidArgumentException $e){
+            return $e->getMessage();
+        }
+        catch(\UnexpectedValueException $e){
+            return $e->getMessage();
+        }
+
+    }
+}
